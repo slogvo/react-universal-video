3694c1f47dfac14a03b58b3248dc22b36be836e5
slogvo <volonglqd@gmail.com>
2024-12-28 13:01:53 +0700

    update new config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
@@ -14,5 +15,22 @@ export default defineConfig({
       "@ui": path.resolve(__dirname, "./src/components/ui"),
     },
   },
-  server: { port: 3030 },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es", "umd"],
+      name: "MyReactLibrary",
+      fileName: (format) => `my-react-library.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  server: { port: 3000 },
 });
